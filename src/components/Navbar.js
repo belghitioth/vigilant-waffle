cd94ab791412c3c247c17f2d0dc741053462d0ae
othmaneeb <belghitioth@gmail.com>
2019-08-05 00:03:07 +0200

    Remove unused import from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,8 @@
 import React, {Component} from 'react';
 
-import Button from "@material-ui/core/Button";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import FormControl from "react-bootstrap/es/FormControl";
 import {Link} from "react-router-dom";
 
