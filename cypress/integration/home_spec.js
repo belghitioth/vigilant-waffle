bc60cdb9bf53c7e90ba3d67cc1914dd61cad7bec
othmaneeb <belghitioth@gmail.com>
2019-08-05 01:11:37 +0200

    Update Cypress tests

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,21 +1,26 @@
 describe('Visit Home Page', function() {
     it('Render home page', function() {
         cy.visit('http://localhost:3000/#/')
+        cy.get('.navbar-form')
     })
-    it('Find Buzzlink title', function() {
+
+    it('Find The DJ hub button ', function() {
         cy.visit('http://localhost:3000/#/')
-        cy.contains('Buzzlink')
-        // Rajouter un id : "data-cypress-home-id"="home-message" (HTML 5 compliant)
+        cy.get('.button > a')        // Rajouter un id : "data-cypress-home-id"="home-message" (HTML 5 compliant)
     })
 })
+
 describe('Visit Venues\' Page', function() {
     it('Render Venues\' page', function() {
-        cy.visit('http://localhost:3000/artists/#/')
+        cy.visit('http://localhost:3000/#/djs/')
+        cy.get('.MuiCardActions-root > :nth-child(1) > .MuiButton-label')
     })
 })
+
 describe('Visit Artists\' Page', function() {
     it('Render Artists\' page', function() {
-        cy.visit('http://localhost:3000/venues/#/')
+        cy.visit('http://localhost:3000/#/gigs/')
+        cy.get('.MuiCardActions-root > :nth-child(1) > .MuiButton-label')
     })
 })
 
