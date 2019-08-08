53e608b904bfafaa2b21f567e4271f2eaa1c8900
othmaneeb <belghitioth@gmail.com>
2019-08-08 10:53:31 +0200

    Separate front and back

---
+++ back-thedjhub/src/main/java/com/back/thedjhub/ThedjhubApplication.java
@@ -0,0 +1,13 @@
+package com.back.thedjhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThedjhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThedjhubApplication.class, args);
+	}
+
+}
